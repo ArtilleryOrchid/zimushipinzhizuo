503b7b5d9e70817bbb33f4a106ad97b0b26b559a
huopaolan <qr896035@163.com>
2020-04-05 02:21:23 +0800

    启屏页 2020:3.5

---
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/home/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.keyi.zimushipinzhizuo.ui.activity.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huopaolan.lib_core.Base.BaseActivity;
+import com.keyi.zimushipinzhizuo.R;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    public int layoutID() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setUpDagger() {
+
+    }
+
+    @Override
+    public void showDialog() {
+
+    }
+
+    @Override
+    public void hideDialog() {
+
+    }
+}
--- app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/login/SplashActivity.java
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/login/SplashActivity.java
@@ -1,12 +1,13 @@
 package com.keyi.zimushipinzhizuo.ui.activity.login;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
 
 import com.huopaolan.lib_core.Base.BaseActivity;
 import com.keyi.zimushipinzhizuo.R;
+import com.keyi.zimushipinzhizuo.ui.activity.home.HomeActivity;
 
 public class SplashActivity extends BaseActivity {
+    private Thread myThread;
 
     @Override
     public int layoutID() {
@@ -15,12 +16,24 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        myThread = new Thread() {//创建子线程
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);//使程序休眠一秒
+                    Intent it = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(it);
+                    finish();//关闭当前活动
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     @Override
     public void initData() {
-
+        myThread.start();//启动线程
     }
 
     @Override
