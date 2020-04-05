c8661de65c00616586dbfa7dc1aa8f82f0c168cd
Dqc117266 <1172662142@qq.com>
2020-04-05 19:07:25 +0800

    时间：2020/4/6 19:08 修改：添加自定义声明弹框dialog

--- app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/home/HomeActivity.java
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/home/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.keyi.zimushipinzhizuo.ui.activity.home;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+import android.content.DialogInterface;
+import android.widget.Toast;
 
 import com.huopaolan.lib_core.Base.BaseActivity;
 import com.keyi.zimushipinzhizuo.R;
+import com.keyi.zimushipinzhizuo.ui.activity.login.SplashActivity;
+import com.keyi.zimushipinzhizuo.ui.widget.dialog.CustomDialog;
 
 public class HomeActivity extends BaseActivity {
 
@@ -15,7 +18,27 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        CustomDialog.Builder builder = new CustomDialog.Builder(HomeActivity.this);
+        builder.setMessage(getBaseContext().getString(R.string.dialog_content));
+        builder.setTitle(getBaseContext().getString(R.string.dialog_title));
 
+        builder.setPositiveButton(getBaseContext().getString(R.string.dialog_agree_btn_text)
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "同意", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        builder.setNegativeButton(getBaseContext().getString(R.string.dialog_disagree_tv_text)
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "拒绝", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        builder.create().show();
     }
 
     @Override
--- app/src/main/java/com/keyi/zimushipinzhizuo/ui/widget/dialog/CustomDialog.java
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/widget/dialog/CustomDialog.java
@@ -2,8 +2,21 @@ package com.keyi.zimushipinzhizuo.ui.widget.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.keyi.zimushipinzhizuo.R;
+
+import org.w3c.dom.Text;
 
 public class CustomDialog extends Dialog {
 
@@ -15,7 +28,111 @@ public class CustomDialog extends Dialog {
         super(context, themeResId);
     }
 
-    protected CustomDialog(@NonNull Context context, boolean cancelable, @Nullable OnCancelListener cancelListener) {
-        super(context, cancelable, cancelListener);
+    public static class Builder {
+        private Context context;
+        private String title;
+        private String message;
+        private String positiveButtonText;
+        private String negativeButtonText;
+        private View contentView;
+        private DialogInterface.OnClickListener postiviteButtonClickLisener;
+        private DialogInterface.OnClickListener negativaButtonClickListener;
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setContentView(View view) {
+            this.contentView = view;
+            return this;
+        }
+
+        public Builder setPositiveButton(String positiveButtonText
+                , DialogInterface.OnClickListener listener) {
+            this.positiveButtonText = positiveButtonText;
+            this.postiviteButtonClickLisener = listener;
+            return this;
+        }
+
+        public Builder setNegativeButton(String negativeButtonText
+                , DialogInterface.OnClickListener listener) {
+            this.negativeButtonText = negativeButtonText;
+            this.negativaButtonClickListener = listener;
+            return this;
+        }
+
+        public CustomDialog create() {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final CustomDialog dialog = new CustomDialog(context, R.style.myDialog);
+            dialog.setCancelable(false);
+            View layout = inflater.inflate(R.layout.dialog_normal_layout, null);
+            dialog.addContentView(layout, new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            ((TextView) layout.findViewById(R.id.title)).setText(title);
+            if (positiveButtonText != null) {
+                ((Button)(layout.findViewById(R.id.positive_btn)))
+                        .setText(positiveButtonText);
+                if (postiviteButtonClickLisener != null) {
+                    ((Button)(layout.findViewById(R.id.positive_btn)))
+                            .setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    postiviteButtonClickLisener.onClick(dialog,
+                                            DialogInterface.BUTTON_POSITIVE);
+                                }
+                            });
+                }
+            } else {
+                layout.findViewById(R.id.positive_btn).setVisibility(View.GONE);
+            }
+
+            if (negativeButtonText != null) {
+                ((TextView) (layout.findViewById(R.id.negative_btn)))
+                        .setText(negativeButtonText);
+                if (negativaButtonClickListener != null) {
+                    ((TextView) (layout.findViewById(R.id.negative_btn)))
+                            .setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    negativaButtonClickListener.onClick(dialog,
+                                            DialogInterface.BUTTON_NEGATIVE);
+                                }
+                            });
+                }
+            } else {
+                layout.findViewById(R.id.negative_btn)
+                        .setVisibility(View.GONE);
+            }
+
+            if (message != null) {
+                ((TextView) (layout.findViewById(R.id.message)))
+                        .setText(message);
+            } else  if (contentView != null) {
+                ((LinearLayout) (layout.findViewById(R.id.content)))
+                        .removeAllViews();
+                ((LinearLayout) (layout.findViewById(R.id.content)))
+                        .addView(contentView
+                                , new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
+            } else {
+                ((TextView) (layout.findViewById(R.id.message)))
+                        .setVisibility(View.GONE);
+            }
+            dialog.setContentView(layout);
+
+            return dialog;
+        }
+
     }
+
 }
