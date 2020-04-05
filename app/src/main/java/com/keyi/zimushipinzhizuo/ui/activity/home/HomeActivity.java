a0a8d6060f32a0652cae41500a7f21691505f22a
huopaolan <qr896035@163.com>
2020-04-05 15:55:36 +0800

    首页部分 2020:3.5

--- app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/home/HomeActivity.java
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/home/HomeActivity.java
@@ -2,9 +2,12 @@ package com.keyi.zimushipinzhizuo.ui.activity.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import com.huopaolan.lib_core.Base.BaseActivity;
 import com.keyi.zimushipinzhizuo.R;
+import com.wangjie.shadowviewhelper.ShadowProperty;
+import com.wangjie.shadowviewhelper.ShadowViewHelper;
 
 public class HomeActivity extends BaseActivity {
 
@@ -15,7 +18,11 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        ShadowViewHelper.bindShadowHelper(
+                new ShadowProperty()
+                        .setShadowDx(0x77FF0000)
+                        .setShadowDy(3)
+                , findViewById(R.id.shadow_back));
     }
 
     @Override
