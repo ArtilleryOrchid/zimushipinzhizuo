b886a852343d1e34eee97de7ba507aafbe609776
huopaolan <qr896035@163.com>
2020-04-13 01:05:11 +0800

    添加文件 2020:4.13

--- app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/mine/HtmlActivity.java
+++ app/src/main/java/com/keyi/zimushipinzhizuo/ui/activity/mine/HtmlActivity.java
@@ -1,9 +1,14 @@
 package com.keyi.zimushipinzhizuo.ui.activity.mine;
 
 import android.content.Intent;
+import android.webkit.ValueCallback;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebSettings.LayoutAlgorithm;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 import com.huopaolan.lib_core.Base.BaseActivity;
 import com.keyi.zimushipinzhizuo.R;
 
@@ -24,6 +29,7 @@ public class HtmlActivity extends BaseActivity {
         String html = intent.getStringExtra("html");
         //支持js交互
         web_html.getSettings().setJavaScriptEnabled(true);
+        web_html.getSettings().setDomStorageEnabled(true);
         //自适应屏幕
         web_html.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
         web_html.getSettings().setLoadWithOverviewMode(true);
@@ -31,6 +37,12 @@ public class HtmlActivity extends BaseActivity {
         switch (html) {
             case "user_agreement":
                 web_html.loadUrl(user_agreement);
+                web_html.evaluateJavascript("javascript:setAppName(\'" + "视频制作" + "\')", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        Toast.makeText(HtmlActivity.this, "" + value, Toast.LENGTH_SHORT).show();
+                    }
+                });
                 break;
             case "privacy_policy":
                 web_html.loadUrl(privacy_policy);
